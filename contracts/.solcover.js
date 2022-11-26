1f130b7e49e129c611cdc387697762a435256663
Keon <kwk236@gmail.com>
2022-11-26 23:21:11 +0900

    update contracts

--- contracts/.solcover.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  skipFiles: [
-    "test",
-    "bls/lib",
-    //solc-coverage fails to compile our Manager module.
-    "gnosis",
-    "samples/SimpleWalletForTokens.sol"
-  ],
-};
